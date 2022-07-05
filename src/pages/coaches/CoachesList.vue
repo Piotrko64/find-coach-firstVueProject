4440434004cf8c8c563e9f76ddc0e4deb730882a
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2022-07-05 15:59:25 +0200

    create basic of /coaches page

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -5,8 +5,46 @@
       <button>REFRESH</button>
       <router-link to="/register">REGISTER AS COACH</router-link>
     </div>
-    LIST
+    <ul v-if="hasCoaches">
+      <coach-item
+        v-for="coach in filteredCoaches"
+        :key="coach.id"
+        :id="coach.id"
+        :firstName="coach.firstName"
+        :lastName="coach.lastName"
+        :rate="coach.hourlyRate"
+        :areas="coach.areas"
+      >
+      </coach-item>
+    </ul>
+    <h3 v-else>No Coaches Found</h3>
   </section>
 </template>
-<script></script>
-<style></style>
+<script>
+import CoachItem from '../../components/coaches/CoachItem.vue';
+export default {
+  components: {
+    CoachItem,
+  },
+  computed: {
+    filteredCoaches() {
+      return this.$store.getters['coaches/coaches'];
+    },
+    hasCoaches() {
+      return this.$store.getters['coaches/hasCoaches'];
+    },
+  },
+};
+</script>
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
