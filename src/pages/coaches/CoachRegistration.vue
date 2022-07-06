9a6be54d3c8ba7b963e94f4053a4045fb5875d0e
Front Flex <77500425+Piotrko64@users.noreply.github.com>
2022-07-06 16:28:54 +0200

    create registartion form

--- src/pages/coaches/CoachRegistration.vue
+++ src/pages/coaches/CoachRegistration.vue
@@ -1,9 +1,24 @@
 <template>
   <section>
-    <h2>Register as a coach now</h2>
+    <base-card>
+      <h2>Register as a coach now</h2>
+      <coach-form @save-data="saveData"></coach-form>
+    </base-card>
   </section>
 </template>
 <script>
-export default {};
+import CoachForm from '../../components/coaches/CoachForm.vue';
+export default {
+  components: {
+    CoachForm,
+  },
+  methods: {
+    saveData(data) {
+      console.log(this.$store);
+      this.$store.dispatch('coaches/registerCoach', data);
+      this.$router.push('/');
+    },
+  },
+};
 </script>
-<style></style>
+<style scoped></style>
